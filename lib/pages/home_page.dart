305b16eaa04d7ff38627a017527ea297b44c46f0
KR3 <fookkingkub@gmail.com>
2025-08-21 16:30:56 +0700

    ss

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,3 @@
-import 'package:ai_task_project_manager/pages/setting/setting_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
